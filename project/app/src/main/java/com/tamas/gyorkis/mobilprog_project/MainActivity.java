ea637de563e82db377fe62290ca7ef097750ae35
gyorkistamas <tamasgyorkis@gmail.com>
2023-10-30 18:32:19 +0100

    implicit intent

--- project/app/src/main/java/com/tamas/gyorkis/mobilprog_project/MainActivity.java
+++ project/app/src/main/java/com/tamas/gyorkis/mobilprog_project/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityResultLauncher<Intent> activityResultLauncher;
 
+    //TODO fekvő layout
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
                     if (o.getResultCode() == RESULT_OK) {
                         Intent intent = o.getData();
                         int age = intent.getIntExtra("age", -1);
-                        Toast.makeText(MainActivity.this, String.format("%s%s", getResources().getString(R.string.your_age), age), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, String.format("%s: %s", getResources().getString(R.string.your_age), age), Toast.LENGTH_SHORT).show();
                     }
                     else {
                         Toast.makeText(MainActivity.this, R.string.error, Toast.LENGTH_SHORT).show();
@@ -37,6 +38,12 @@ public class MainActivity extends AppCompatActivity {
         Button explicit = findViewById(R.id.btnStartExplicit);
         explicit.setOnClickListener((v) -> {
             Intent intent = new Intent(MainActivity.this, ParameterActivity.class);
+            activityResultLauncher.launch(intent);
+        });
+
+        Button implicit = findViewById(R.id.btnStartImplicit);
+        implicit.setOnClickListener((v) -> {
+            Intent intent = new Intent("com.tamas.gyorkis.mobilprog_project.ASKBIRTHDAY");
             activityResultLauncher.launch(intent);
         });
     }
